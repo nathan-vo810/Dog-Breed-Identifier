42378d7739cc882be6261efac4c4ac616bad61cf
anhvo <anh.vnn810@gmail.com>
2020-10-26 22:35:47 -0400

    Refactor code

--- train_model.py
+++ train_model.py
@@ -14,25 +14,24 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision
 from torchvision import transforms, datasets, models
 import copy
+import argparse
 
-# Random seed
-np.random.seed(0)
+def prepare_data():
+	data_transform = transforms.Compose([transforms.Scale(224),
+	                               transforms.CenterCrop(224),
+	                               transforms.ToTensor(),
+	                               transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
-data_transform = transforms.Compose([transforms.Scale(224),
-                               transforms.CenterCrop(224),
-                               transforms.ToTensor(),
-                               transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+	data_dir = './dataset'
 
-data_dir = './dataset'
+	image_datasets = {x: datasets.ImageFolder(join(data_dir, x), data_transform) for x in ['train', 'valid']}
 
-image_datasets = {x: datasets.ImageFolder(join(data_dir, x), data_transform) for x in ['train', 'valid']}
+	dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,shuffle=True, num_workers=4) for x in ['train', 'valid']}
 
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,shuffle=True, num_workers=4) for x in ['train', 'valid']}
+	dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'valid']}
+	class_names = image_datasets['train'].classes
 
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'valid']}
-class_names = image_datasets['train'].classes
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+	return dataloaders, dataset_sizes, class_names
 
 def imshow(inp, title=None):
     """Imshow for Tensor."""
@@ -46,7 +45,7 @@ def imshow(inp, title=None):
         plt.title(title)
     plt.pause(0.001)  # pause a bit so that plots are updated
 
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+def train_model(device, model, dataloaders, dataset_sizes, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
     
     best_model_wts = copy.deepcopy(model.state_dict())
@@ -114,20 +113,61 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     model.load_state_dict(best_model_wts)
     return model
 
-model = models.wide_resnet50_2(pretrained=True)
-for param in model.parameters():
-    param.requires_grad = False
+def get_args():
+	# # Create parser object
+    parser = argparse.ArgumentParser(description="CLI for Dog Breed Classifier")
 
-num_features = model.fc.in_features
+    parser.add_argument('-model', type=str, nargs=1,
+                        metavar="model", default='ResNet18', choices={'ResNet18', 'ResNet50', 'Wide_ResNet', 'Inception_v3'},
+                        help='Choose model type - ResNet18/ResNet50/Wide_ResNet/Inception_v3')
 
-model.fc = torch.nn.Linear(num_features, len(class_names))
+    parser.add_argument('-num_epochs', type=int,
+    					metavar="num_epochs", default=1)
+    args = parser.parse_args()
+    return args
 
-model = model.to(device)
 
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.fc.parameters(), lr=0.001, momentum=0.9)
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+def main():
+	# Get arguments
+	args = get_args()
 
-my_model = train_model(model, criterion, optimizer, exp_lr_scheduler, num_epochs=30)
+	# Generate random seed
+	np.random.seed(0)
 
-torch.save(my_model.state_dict(), "./best_weights.pth")
+	# Prepare device
+	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+	# Prepare Data
+	dataloaders, dataset_sizes, class_names = prepare_data()
+
+	# Prepare Model
+	print("Using Model ", args.model)
+	if args.model == 'ResNet18':
+		model = models.resnet18(pretrained=True)
+	elif args.model == 'ResNet50':	
+		model = models.resnet50(pretrained=True)
+	elif args.model == 'Inception_v3':
+		model = models.inception_v3(pretrained=True)
+	else:
+		model = models.wide_resnet50_2(pretrained=True)
+	for param in model.parameters():
+	    param.requires_grad = False
+
+	num_features = model.fc.in_features
+
+	model.fc = torch.nn.Linear(num_features, len(class_names))
+
+	model = model.to(device)
+
+	criterion = torch.nn.CrossEntropyLoss()
+	optimizer = torch.optim.SGD(model.fc.parameters(), lr=0.001, momentum=0.9)
+	exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+	# Train Model
+	my_model = train_model(device, model, dataloaders, dataset_sizes, criterion, optimizer, exp_lr_scheduler, num_epochs=args.num_epochs)
+
+	# Save Model
+	torch.save(my_model.state_dict(), "./best_weights.pth")
+
+if __name__ == '__main__':
+	main()
